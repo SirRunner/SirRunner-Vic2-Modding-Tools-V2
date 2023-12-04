01db08fd5072bc4c60c0efd7daad98574b9c3058
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-12-04 00:07:35 -0600

    Add birth and death event generation

--- src/main/java/events/generators/LeaderAgeConfiguration.java
+++ src/main/java/events/generators/LeaderAgeConfiguration.java
@@ -1,8 +1,20 @@
 package events.generators;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum LeaderAgeConfiguration {
-    BARD_I(33000, "bard_i_info", "bard_i_age", "DAL_bard_i_age", 1066, "Bard I", 55, 82),
-    BAIN_I(33001, "bard_i_info", "bain_i_age", "DAL_bain_i_age", 1067, "Bain I", 25, 82);
+    BARD_I(33000, "bard_i_info", "bard_i_age", "DAL_bard_i_age", 1066, "Bard I", 55, 82, ConfigurationType.AGE), // 2898 - 2980
+    BAIN_I(33001, "bard_i_info", "bain_i_age", "DAL_bain_i_age", 1067, "Bain I", 25, 82, ConfigurationType.AGE), // 2928 - 3010
+    BRAND_I(33002, "bard_i_info", "brand_i_age", "DAL_brand_i_age", 1069, "Brand I", -1, 79, ConfigurationType.AGE), // 2954 - 3033
+    BARD_II(33003, "bard_i_info", "bard_ii_age", "DAL_bard_ii_age", 1070, "Bard II", -1, 80, ConfigurationType.AGE), // 2982 - 3062
+    BRAND_I_BIRTH(33004, "bard_i_info", "DAL_brand_i_born", "DAL_brand_i_age", 1071, "Brand I", 2954, 2960, ConfigurationType.BIRTH), // Official birth in 2957
+    BARD_II_BIRTH(33005, "bard_i_info", "DAL_bard_ii_born", "DAL_bard_ii_age", 1072, "Bard II", 2982, 2988, ConfigurationType.BIRTH), // Official birth in 2985
+    BARD_I_DEATH(33006, "bard_i_info", "bard_i_age", "DAL_bard_i_age", 1066, "Bard I", 2974, 2980, ConfigurationType.DEATH), // Official death in 2977
+    BAIN_I_DEATH(33007, "bard_i_info", "bain_i_age", "DAL_bain_i_age", 1067, "Bain I", 3004, 3010, ConfigurationType.DEATH), // Official death in 3007
+    BRAND_I_DEATH(33008, "bard_i_info", "brand_i_age", "DAL_brand_i_age", 1069, "Brand I", 3018, 3033, ConfigurationType.DEATH), // Official death in 3019
+    BARD_II_DEATH(33009, "bard_i_info", "bard_ii_age", "DAL_bard_ii_age", 1070, "Bard II", 3056, 3062, ConfigurationType.DEATH); // Official death in FA 40 (TA 3059)
 
     private final int id;
     private final String picture;
@@ -12,8 +24,9 @@ public enum LeaderAgeConfiguration {
     private final String text;
     private final int startAge;
     private final int maxAge;
+    private final ConfigurationType type;
 
-    LeaderAgeConfiguration(int id, String picture, String variable, String region, int province, String text, int startAge, int maxAge) {
+    LeaderAgeConfiguration(int id, String picture, String variable, String region, int province, String text, int startAge, int maxAge, ConfigurationType type) {
         this.id = id;
         this.picture = picture;
         this.variable = variable;
@@ -22,6 +35,7 @@ public enum LeaderAgeConfiguration {
         this.text = text;
         this.startAge = startAge;
         this.maxAge = maxAge;
+        this.type = type;
     }
 
     public int getId() {
@@ -54,5 +68,19 @@ public enum LeaderAgeConfiguration {
 
     public int getMaxAge() {
         return maxAge;
+    }
+
+    public ConfigurationType getType() {
+        return type;
+    }
+
+    public static List<LeaderAgeConfiguration> getConfigurationsByType(ConfigurationType type) {
+        return Arrays.stream(values()).filter(config -> type == config.getType()).collect(Collectors.toList());
+    }
+
+    public enum ConfigurationType {
+        AGE,
+        BIRTH,
+        DEATH
     }
 }
--- src/main/java/events/generators/LeaderAgeGenerator.java
+++ src/main/java/events/generators/LeaderAgeGenerator.java
@@ -11,8 +11,11 @@ import utils.paradox.scripting.effects.EffectScope;
 
 import java.io.FileWriter;
 import java.nio.charset.Charset;
+import java.time.Month;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class LeaderAgeGenerator {
 
@@ -30,7 +33,7 @@ public class LeaderAgeGenerator {
 
         List<Event> events = new ArrayList<>();
 
-        for (LeaderAgeConfiguration leaderAgeConfiguration : LeaderAgeConfiguration.values()) {
+        for (LeaderAgeConfiguration leaderAgeConfiguration : LeaderAgeConfiguration.getConfigurationsByType(LeaderAgeConfiguration.ConfigurationType.AGE)) {
             Event event = new Event();
 
             event.setId(leaderAgeConfiguration.getId());
@@ -71,15 +74,123 @@ public class LeaderAgeGenerator {
 
             option.addEffect(returnScope);
 
+            /* All to make sure that the event stops looping when a leader dies -- -2 = death, -1 = not born */
+            EffectScope randomOwned = ScriptingUtils.getEffectScope("random_owned");
+            ConditionScope limit = ScriptingUtils.getConditionScope("limit");
+            ConditionScope ownerCondition = ScriptingUtils.getConditionScope("owner");
+            ConditionScope FOR = ScriptingUtils.getConditionScope("FOR");
+
+            ConditionScope lowerBound = ScriptingUtils.getConditionScope("check_variable");
+            Condition LBvariable = ScriptingUtils.getCondition("which", leaderAgeConfiguration.getVariable());
+            Condition LBvalue = ScriptingUtils.getCondition("value", "-0.1");
+            lowerBound.addCondition(LBvariable);
+            lowerBound.addCondition(LBvalue);
+
+            FOR.addCondition(lowerBound);
+            ownerCondition.addCondition(FOR);
+            limit.addCondition(ownerCondition);
+            randomOwned.setLimit(limit);
+
+            EffectScope owner = ScriptingUtils.getEffectScope("owner");
             EffectScope loopScope = ScriptingUtils.getEffectScope("country_event");
             loopScope.addEffect(ScriptingUtils.getEffect("id", Integer.toString(leaderAgeConfiguration.getId())));
             loopScope.addEffect(ScriptingUtils.getEffect("days", "365"));
 
-            option.addEffect(loopScope);
+            owner.addEffect(loopScope);
+            randomOwned.addEffect(owner);
+            option.addEffect(randomOwned);
 
             event.addOption(option);
 
             events.add(event);
+        }
+
+        for (LeaderAgeConfiguration leaderAgeConfiguration : LeaderAgeConfiguration.getConfigurationsByType(LeaderAgeConfiguration.ConfigurationType.BIRTH)) {
+            Event event = new Event();
+
+            event.setId(leaderAgeConfiguration.getId());
+            event.setPicture(leaderAgeConfiguration.getPicture());
+            event.setTitle("Setting " + leaderAgeConfiguration.getText() + "'s Birth Month and Year");
+            event.setDescription("");
+            event.setTriggeredOnly(true);
+
+            Option option = new Option();
+            option.setName("Update please");
+
+            EffectScope secedeScope = ScriptingUtils.getEffectScope(Integer.toString(leaderAgeConfiguration.getProvince()));
+            secedeScope.addEffect(ScriptingUtils.getEffect("secede_province", "THIS"));
+
+            option.addEffect(secedeScope);
+
+            /* Start Age = first year leader could be born. Max Age = last year leader could be born */
+            for (int i = leaderAgeConfiguration.getStartAge(); i <= leaderAgeConfiguration.getMaxAge(); i++) {
+                for (int month = 1; month <= 12; month++) {
+                    option.addEffect(getBirthUpdateEffect(i, month, leaderAgeConfiguration));
+                }
+            }
+
+            EffectScope returnScope = ScriptingUtils.getEffectScope(Integer.toString(leaderAgeConfiguration.getProvince()));
+            Effect secede = ScriptingUtils.getEffect("secede_province", "---");
+            secede.setComment("Audax Validator \".\" Ignore_NEXT");
+            secede.setOneLineOverride(false);
+            returnScope.setOneLineOverride(false);
+            returnScope.addEffect(secede);
+
+            option.addEffect(returnScope);
+
+            event.addOption(option);
+
+            events.add(event);
+        }
+
+        for (LeaderAgeConfiguration leaderAgeConfiguration: LeaderAgeConfiguration.getConfigurationsByType(LeaderAgeConfiguration.ConfigurationType.DEATH)) {
+            Event event = new Event();
+
+            event.setId(leaderAgeConfiguration.getId());
+            event.setPicture(leaderAgeConfiguration.getPicture());
+            event.setTitle("Setting " + leaderAgeConfiguration.getText() + "'s Death Month and Year");
+            event.setDescription("");
+            event.setTriggeredOnly(true);
+
+            Option option = new Option();
+            option.setName("Update please");
+
+            EffectScope variableTag = ScriptingUtils.getEffectScope("FOR");
+            EffectScope setVariable = ScriptingUtils.getEffectScope("set_variable");
+
+            Effect which = ScriptingUtils.getEffect("which", leaderAgeConfiguration.getVariable());
+            Effect value = ScriptingUtils.getEffect("value", "-3");
+            setVariable.addEffect(which);
+            setVariable.addEffect(value);
+            variableTag.addEffect(setVariable);
+
+            option.addEffect(variableTag);
+
+            EffectScope secedeScope = ScriptingUtils.getEffectScope(Integer.toString(leaderAgeConfiguration.getProvince()));
+            secedeScope.addEffect(ScriptingUtils.getEffect("secede_province", "THIS"));
+
+            option.addEffect(secedeScope);
+
+            /* Start Age = first year leader could die. Max Age = last year leader could die */
+            for (int i = leaderAgeConfiguration.getStartAge(); i <= leaderAgeConfiguration.getMaxAge(); i++) {
+                for (int month = 1; month <= 12; month++) {
+                    option.addEffect(getDeathUpdateEffect(i, month, leaderAgeConfiguration));
+                }
+            }
+
+            EffectScope returnScope = ScriptingUtils.getEffectScope(Integer.toString(leaderAgeConfiguration.getProvince()));
+            Effect secede = ScriptingUtils.getEffect("secede_province", "---");
+            secede.setComment("Audax Validator \".\" Ignore_NEXT");
+            secede.setOneLineOverride(false);
+            returnScope.setOneLineOverride(false);
+            returnScope.addEffect(secede);
+
+            option.addEffect(returnScope);
+
+            event.addOption(option);
+
+            events.add(event);
+
         }
 
         try (FileWriter writer = new FileWriter(getEventsFilename(), Charset.forName("windows-1252"))) {
@@ -100,16 +211,32 @@ public class LeaderAgeGenerator {
         ConditionScope ownerCondition = ScriptingUtils.getConditionScope("owner");
         ConditionScope FOR = ScriptingUtils.getConditionScope("FOR");
 
+        String lowerBoundValue = (age - 1) + ".9";
+
+        if (age < 0) {
+            lowerBoundValue = age + ".1";
+        } else if (age == 0) {
+            lowerBoundValue = "-0.1";
+        }
+
         ConditionScope lowerBound = ScriptingUtils.getConditionScope("check_variable");
         Condition LBvariable = ScriptingUtils.getCondition("which", leaderAgeConfiguration.getVariable());
-        Condition LBvalue = ScriptingUtils.getCondition("value", (age - 1) + ".9");
+        Condition LBvalue = ScriptingUtils.getCondition("value", lowerBoundValue);
         lowerBound.addCondition(LBvariable);
         lowerBound.addCondition(LBvalue);
+
+        String upperBoundValue = age + ".1";
+
+        if (age < -1) {
+            upperBoundValue = (age + 1) + ".9";
+        } else if (age == -1) {
+            upperBoundValue = "-0.9";
+        }
 
         ConditionScope NOT = ScriptingUtils.getNOTCondition();
         ConditionScope upperBound = ScriptingUtils.getConditionScope("check_variable");
         Condition UBvariable = ScriptingUtils.getCondition("which", leaderAgeConfiguration.getVariable());
-        Condition UBvalue = ScriptingUtils.getCondition("value", age + ".1");
+        Condition UBvalue = ScriptingUtils.getCondition("value", upperBoundValue);
         upperBound.addCondition(UBvariable);
         upperBound.addCondition(UBvalue);
         NOT.addCondition(upperBound);
@@ -122,12 +249,95 @@ public class LeaderAgeGenerator {
         region.setLimit(limit);
 
         EffectScope stateScope = ScriptingUtils.getEffectScope("state_scope");
-        Effect changeRegionName = ScriptingUtils.getEffect("change_region_name", "\"§Y" + leaderAgeConfiguration.getText() + "§! is currently §Y" + age + "§! years old\"");
+
+        String text = "\"§Y" + leaderAgeConfiguration.getText() + "§! is currently §Y" + age + "§! years old§I\"";
+
+        if (age == -1) {
+            text = "\"§Y" + leaderAgeConfiguration.getText() + "§! has not yet been born§I\"";
+        }
+
+        Effect changeRegionName = ScriptingUtils.getEffect("change_region_name", text);
 
         stateScope.addEffect(changeRegionName);
         region.addEffect(stateScope);
 
         return region;
+    }
+
+    protected BasicEffect getBirthUpdateEffect(int year, int monthInt, LeaderAgeConfiguration configuration) {
+
+        Month month = Month.of(monthInt);
+
+        EffectScope randomOwned = ScriptingUtils.getEffectScope("random_owned");
+
+        ConditionScope limit = ScriptingUtils.getConditionScope("limit");
+        Condition provinceId = ScriptingUtils.getCondition("province_id", Integer.toString(configuration.getProvince()));
+        ConditionScope owner = ScriptingUtils.getConditionScope("owner");
+        Condition currentYear = ScriptingUtils.getCondition("year", Integer.toString(year));
+        ConditionScope notYear = ScriptingUtils.getNOTCondition();
+        Condition nextYear = ScriptingUtils.getCondition("year", Integer.toString(year + 1));
+        Condition currentMonth = ScriptingUtils.getCondition("month", Integer.toString(monthInt - 1));
+        ConditionScope notMonth = ScriptingUtils.getNOTCondition();
+        Condition nextMonth = ScriptingUtils.getCondition("month", Integer.toString(monthInt));
+
+        EffectScope stateScope = ScriptingUtils.getEffectScope("state_scope");
+        Effect changeRegionName = ScriptingUtils.getEffect("change_region_name", "\"§Y" + configuration.getText() + "§! was born in §Y" + month.getDisplayName(TextStyle.FULL, Locale.ENGLISH) +"§! of §YTA " + year + "§!§I\"");
+
+        notYear.addCondition(nextYear);
+        notMonth.addCondition(nextMonth);
+
+        owner.addCondition(currentYear);
+        owner.addCondition(notYear);
+        owner.addCondition(currentMonth);
+        owner.addCondition(notMonth);
+
+        limit.addCondition(provinceId);
+        limit.addCondition(owner);
+
+        stateScope.addEffect(changeRegionName);
+        randomOwned.addEffect(stateScope);
+
+        randomOwned.setLimit(limit);
+
+        return randomOwned;
+    }
+
+    protected BasicEffect getDeathUpdateEffect(int year, int monthInt, LeaderAgeConfiguration configuration) {
+
+        Month month = Month.of(monthInt);
+
+        EffectScope randomOwned = ScriptingUtils.getEffectScope("random_owned");
+
+        ConditionScope limit = ScriptingUtils.getConditionScope("limit");
+        Condition provinceId = ScriptingUtils.getCondition("province_id", Integer.toString(configuration.getProvince()));
+        ConditionScope owner = ScriptingUtils.getConditionScope("owner");
+        Condition currentYear = ScriptingUtils.getCondition("year", Integer.toString(year));
+        ConditionScope notYear = ScriptingUtils.getNOTCondition();
+        Condition nextYear = ScriptingUtils.getCondition("year", Integer.toString(year + 1));
+        Condition currentMonth = ScriptingUtils.getCondition("month", Integer.toString(monthInt - 1));
+        ConditionScope notMonth = ScriptingUtils.getNOTCondition();
+        Condition nextMonth = ScriptingUtils.getCondition("month", Integer.toString(monthInt));
+
+        EffectScope stateScope = ScriptingUtils.getEffectScope("state_scope");
+        Effect changeRegionName = ScriptingUtils.getEffect("change_region_name", "\"§Y" + configuration.getText() + "§! died in §Y" + month.getDisplayName(TextStyle.FULL, Locale.ENGLISH) +"§! of §YTA " + year + "§!§I\"");
+
+        notYear.addCondition(nextYear);
+        notMonth.addCondition(nextMonth);
+
+        owner.addCondition(currentYear);
+        owner.addCondition(notYear);
+        owner.addCondition(currentMonth);
+        owner.addCondition(notMonth);
+
+        limit.addCondition(provinceId);
+        limit.addCondition(owner);
+
+        stateScope.addEffect(changeRegionName);
+        randomOwned.addEffect(stateScope);
+
+        randomOwned.setLimit(limit);
+
+        return randomOwned;
     }
 
     public static void main(String[] args) {
