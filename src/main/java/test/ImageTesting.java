0feca8367e7ef2208475d90b65895700ec3a4e1c
SirRunner <33076111+SirRunner@users.noreply.github.com>
2024-05-18 02:03:24 -0500

    Fillable province map generator

---
+++ src/main/java/test/ImageTesting.java
@@ -0,0 +1,104 @@
+package test;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ImageTesting {
+
+    public static boolean shouldUpdateColor(int x, int y, boolean checkLeft, boolean checkRight, boolean checkUp, boolean checkDown, Color color, BufferedImage img) throws Exception {
+
+        if (checkLeft && checkRight) {
+            throw new Exception("Invalid configuration passed: left/right");
+        }
+
+        if (checkUp && checkDown) {
+            throw new Exception("Invalid configuration passed: up/down");
+        }
+
+        int xAdjustment = 0;
+        int yAdjustment = 0;
+
+        if (checkLeft) {
+            xAdjustment -= 1;
+        }
+
+        if (checkRight) {
+            xAdjustment += 1;
+        }
+
+        if (checkUp) {
+            yAdjustment -= 1;
+        }
+
+        if (checkDown) {
+            yAdjustment += 1;
+        }
+
+        Color otherColor = new Color(img.getRGB(x + xAdjustment, y + yAdjustment));
+
+        return color.equals(otherColor);
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedImage img = ImageIO.read(new File("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Victoria 2\\mod\\TTA\\test_provinces.bmp"));
+        BufferedImage output = new BufferedImage(img.getWidth(), img.getHeight(), img.getType());
+
+        for (int x = 0; x < img.getWidth(); x++) {
+            for (int y = 0; y < img.getHeight(); y++) {
+
+
+
+                boolean canCheckLeft = x != 0;
+                boolean canCheckRight = x != img.getWidth() - 1;
+                boolean canCheckUp = y != 0;
+                boolean canCheckDown = y != img.getHeight() - 1;
+
+                Color color = new Color(img.getRGB(x, y));
+
+                /*
+                 * By default, a pixel is black. We only want to change the color to white if all adjacent (including corners) on the original image are the same color.
+                 * If even a single pixel is different, we need to keep the pixel on the new image as black to signify a border
+                 */
+                if (canCheckLeft && canCheckUp && !shouldUpdateColor(x, y, true, false, true, false, color, img)) {
+                    continue;
+                }
+
+                if (canCheckUp && !shouldUpdateColor(x, y, false, false, true, false, color, img)) {
+                    continue;
+                }
+
+                if (canCheckRight && canCheckUp && !shouldUpdateColor(x, y, false, true, true, false, color, img)) {
+                    continue;
+                }
+
+                if (canCheckLeft && !shouldUpdateColor(x, y, true, false, false, false, color, img)) {
+                    continue;
+                }
+
+                if (canCheckRight && !shouldUpdateColor(x, y, false, true, false, false, color, img)) {
+                    continue;
+                }
+
+                if (canCheckLeft && canCheckDown && !shouldUpdateColor(x, y, true, false, false, true, color, img)) {
+                    continue;
+                }
+
+                if (canCheckDown && !shouldUpdateColor(x, y, false, false, false, true, color, img)) {
+                    continue;
+                }
+
+                if (canCheckRight && canCheckDown && !shouldUpdateColor(x, y, false, true, false, true, color, img)) {
+                    continue;
+                }
+
+                output.setRGB(x, y, Color.WHITE.getRGB());
+            }
+        }
+
+        ImageIO.write(output, "BMP", new File("C:\\Program Files (x86)\\Steam\\steamapps\\common\\Victoria 2\\mod\\TTA\\test_output.bmp"));
+    }
+}
