1e7bfb40ffdb82bb3acf37141070c961013b87cc
SirRunner <33076111+SirRunner@users.noreply.github.com>
2026-08-14 19:38:12 -0500

    Save file structure #2

--- src/main/java/savefile/V2SaveFile.java
+++ src/main/java/savefile/V2SaveFile.java
@@ -3,6 +3,7 @@ package savefile;
 import savefile.objects.*;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 public class V2SaveFile {
@@ -43,4 +44,405 @@ public class V2SaveFile {
     private NewsCollector newsCollector;
     private CrisisManager crisisManager;
     private List<RegionSaveState> regionStates;
+
+    public V2SaveFile() {
+        init();
+    }
+
+    public void init() {
+        if (globalFlags == null) {
+            globalFlags = new Flags();
+        }
+        if (gamePlaySettings == null) {
+            gamePlaySettings = new GamePlaySettings();
+        }
+        if (worldMarket == null) {
+            worldMarket = new WorldMarket();
+        }
+        if (overseasPenalty == null) {
+            overseasPenalty = new GoodsPool();
+        }
+        if (unitCost == null) {
+            unitCost = new GoodsPool();
+        }
+        if (budgetBalance == null) {
+            budgetBalance = new DoubleList();
+        }
+        if (playerMonthlyPopGrowth == null) {
+            playerMonthlyPopGrowth = new PlayerMonthlyPopGrowth();
+        }
+        if (ideologyEnabledDates == null) {
+            ideologyEnabledDates = new ArrayList<>();
+        }
+        if (id == null) {
+            id = new ID();
+        }
+        if (firedEvents == null) {
+            firedEvents = new FiredEvents();
+        }
+        if (provinceStates == null) {
+            provinceStates = new ArrayList<>();
+        }
+        if (countryStates == null) {
+            countryStates = new ArrayList<>();
+        }
+        if (rebelFactions == null) {
+            rebelFactions = new ArrayList<>();
+        }
+        if (activeRelationships == null) {
+            activeRelationships = new Diplomacy();
+        }
+        if (activeCombats == null) {
+            activeCombats = new Combat();
+        }
+        if (activeWars == null) {
+            activeWars = new ArrayList<>();
+        }
+        if (previousWars == null) {
+            previousWars = new ArrayList<>();
+        }
+        if (inventions == null) {
+            inventions = new IntegerList();
+        }
+        if (greatNations == null) {
+            greatNations = new IntegerList();
+        }
+        if (outliner == null) {
+            outliner = new IntegerList();
+        }
+        if (newsCollector == null) {
+            newsCollector = new NewsCollector();
+        }
+        if (crisisManager == null) {
+            crisisManager = new CrisisManager();
+        }
+        if (regionStates == null) {
+            regionStates = new ArrayList<>();
+        }
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getPlayerTag() {
+        return playerTag;
+    }
+
+    public void setPlayerTag(String playerTag) {
+        this.playerTag = playerTag;
+    }
+
+    public int getGovernmentId() {
+        return governmentId;
+    }
+
+    public void setGovernmentId(int governmentId) {
+        this.governmentId = governmentId;
+    }
+
+    public boolean isAutomateTrade() {
+        return automateTrade;
+    }
+
+    public void setAutomateTrade(boolean automateTrade) {
+        this.automateTrade = automateTrade;
+    }
+
+    public int getAutomateSliders() {
+        return automateSliders;
+    }
+
+    public void setAutomateSliders(int automateSliders) {
+        this.automateSliders = automateSliders;
+    }
+
+    public int getRebel() {
+        return rebel;
+    }
+
+    public void setRebel(int rebel) {
+        this.rebel = rebel;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+
+    public void setUnit(int unit) {
+        this.unit = unit;
+    }
+
+    public int getState() {
+        return state;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public Flags getGlobalFlags() {
+        return globalFlags;
+    }
+
+    public void setGlobalFlags(Flags globalFlags) {
+        this.globalFlags = globalFlags;
+    }
+
+    public GamePlaySettings getGamePlaySettings() {
+        return gamePlaySettings;
+    }
+
+    public void setGamePlaySettings(GamePlaySettings gamePlaySettings) {
+        this.gamePlaySettings = gamePlaySettings;
+    }
+
+    public LocalDate getStartdate() {
+        return startdate;
+    }
+
+    public void setStartdate(LocalDate startdate) {
+        this.startdate = startdate;
+    }
+
+    public int getStartPopIndex() {
+        return startPopIndex;
+    }
+
+    public void setStartPopIndex(int startPopIndex) {
+        this.startPopIndex = startPopIndex;
+    }
+
+    public WorldMarket getWorldMarket() {
+        return worldMarket;
+    }
+
+    public void setWorldMarket(WorldMarket worldMarket) {
+        this.worldMarket = worldMarket;
+    }
+
+    public boolean isGreatWarsEnabled() {
+        return greatWarsEnabled;
+    }
+
+    public void setGreatWarsEnabled(boolean greatWarsEnabled) {
+        this.greatWarsEnabled = greatWarsEnabled;
+    }
+
+    public boolean isWorldWarsEnabled() {
+        return worldWarsEnabled;
+    }
+
+    public void setWorldWarsEnabled(boolean worldWarsEnabled) {
+        this.worldWarsEnabled = worldWarsEnabled;
+    }
+
+    public GoodsPool getOverseasPenalty() {
+        return overseasPenalty;
+    }
+
+    public void setOverseasPenalty(GoodsPool overseasPenalty) {
+        this.overseasPenalty = overseasPenalty;
+    }
+
+    public GoodsPool getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(GoodsPool unitCost) {
+        this.unitCost = unitCost;
+    }
+
+    public DoubleList getBudgetBalance() {
+        return budgetBalance;
+    }
+
+    public void setBudgetBalance(DoubleList budgetBalance) {
+        this.budgetBalance = budgetBalance;
+    }
+
+    public PlayerMonthlyPopGrowth getPlayerMonthlyPopGrowth() {
+        return playerMonthlyPopGrowth;
+    }
+
+    public void setPlayerMonthlyPopGrowth(PlayerMonthlyPopGrowth playerMonthlyPopGrowth) {
+        this.playerMonthlyPopGrowth = playerMonthlyPopGrowth;
+    }
+
+    public String getPlayerMonthlyPopGrowthTag() {
+        return playerMonthlyPopGrowthTag;
+    }
+
+    public void setPlayerMonthlyPopGrowthTag(String playerMonthlyPopGrowthTag) {
+        this.playerMonthlyPopGrowthTag = playerMonthlyPopGrowthTag;
+    }
+
+    public LocalDate getPlayerMonthlyPopGrowthDate() {
+        return playerMonthlyPopGrowthDate;
+    }
+
+    public void setPlayerMonthlyPopGrowthDate(LocalDate playerMonthlyPopGrowthDate) {
+        this.playerMonthlyPopGrowthDate = playerMonthlyPopGrowthDate;
+    }
+
+    public List<IdeologyEnabledDate> getIdeologyEnabledDates() {
+        return ideologyEnabledDates;
+    }
+
+    public void setIdeologyEnabledDates(List<IdeologyEnabledDate> ideologyEnabledDates) {
+        this.ideologyEnabledDates = ideologyEnabledDates;
+    }
+
+    public void addIdeologyEnabledDate(IdeologyEnabledDate ideologyEnabledDate) {
+        this.ideologyEnabledDates.add(ideologyEnabledDate);
+    }
+
+    public ID getId() {
+        return id;
+    }
+
+    public void setId(ID id) {
+        this.id = id;
+    }
+
+    public FiredEvents getFiredEvents() {
+        return firedEvents;
+    }
+
+    public void setFiredEvents(FiredEvents firedEvents) {
+        this.firedEvents = firedEvents;
+    }
+
+    public List<ProvinceSaveState> getProvinceStates() {
+        return provinceStates;
+    }
+
+    public void setProvinceStates(List<ProvinceSaveState> provinceStates) {
+        this.provinceStates = provinceStates;
+    }
+
+    public void addProvinceStatue(ProvinceSaveState provinceSaveState) {
+        this.provinceStates.add(provinceSaveState);
+    }
+
+    public List<CountrySaveState> getCountryStates() {
+        return countryStates;
+    }
+
+    public void setCountryStates(List<CountrySaveState> countryStates) {
+        this.countryStates = countryStates;
+    }
+
+    public void addCountryState(CountrySaveState countrySaveState) {
+        this.countryStates.add(countrySaveState);
+    }
+
+    public List<RebelFaction> getRebelFactions() {
+        return rebelFactions;
+    }
+
+    public void setRebelFactions(List<RebelFaction> rebelFactions) {
+        this.rebelFactions = rebelFactions;
+    }
+
+    public void addRebelFaction(RebelFaction rebelFaction) {
+        this.rebelFactions.add(rebelFaction);
+    }
+
+    public Diplomacy getActiveRelationships() {
+        return activeRelationships;
+    }
+
+    public void setActiveRelationships(Diplomacy activeRelationships) {
+        this.activeRelationships = activeRelationships;
+    }
+
+    public Combat getActiveCombats() {
+        return activeCombats;
+    }
+
+    public void setActiveCombats(Combat activeCombats) {
+        this.activeCombats = activeCombats;
+    }
+
+    public List<War> getActiveWars() {
+        return activeWars;
+    }
+
+    public void setActiveWars(List<War> activeWars) {
+        this.activeWars = activeWars;
+    }
+
+    public void addActiveWar(War activeWar) {
+        this.activeWars.add(activeWar);
+    }
+
+    public List<War> getPreviousWars() {
+        return previousWars;
+    }
+
+    public void setPreviousWars(List<War> previousWars) {
+        this.previousWars = previousWars;
+    }
+
+    public void addPreviousWar(War previousWar) {
+        this.previousWars.add(previousWar);
+    }
+
+    public IntegerList getInventions() {
+        return inventions;
+    }
+
+    public void setInventions(IntegerList inventions) {
+        this.inventions = inventions;
+    }
+
+    public IntegerList getGreatNations() {
+        return greatNations;
+    }
+
+    public void setGreatNations(IntegerList greatNations) {
+        this.greatNations = greatNations;
+    }
+
+    public IntegerList getOutliner() {
+        return outliner;
+    }
+
+    public void setOutliner(IntegerList outliner) {
+        this.outliner = outliner;
+    }
+
+    public NewsCollector getNewsCollector() {
+        return newsCollector;
+    }
+
+    public void setNewsCollector(NewsCollector newsCollector) {
+        this.newsCollector = newsCollector;
+    }
+
+    public CrisisManager getCrisisManager() {
+        return crisisManager;
+    }
+
+    public void setCrisisManager(CrisisManager crisisManager) {
+        this.crisisManager = crisisManager;
+    }
+
+    public List<RegionSaveState> getRegionStates() {
+        return regionStates;
+    }
+
+    public void setRegionStates(List<RegionSaveState> regionStates) {
+        this.regionStates = regionStates;
+    }
+
+    public void addRegionState(RegionSaveState regionState) {
+        this.regionStates.add(regionState);
+    }
 }
